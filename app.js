230c3426eaa349f638c0c355b31d63794588e0d0
casualme7 <miljan.pantovic@gmail.com>
2023-12-12 15:16:26 +0100

    Added rate limiter

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 const appController = require("./controllers/appController");
 const userRouter = require("./routes/userRoutes");
 const purchaseOrdersRouter = require("./routes/purchaseOrdersRoutes");
@@ -8,11 +9,23 @@ const purchaseOrdersRouter = require("./routes/purchaseOrdersRoutes");
 const app = express();
 
 // Middlewares
+// Cors
 app.use(
 	cors({
 		origin: "*",
 	})
 );
+
+// Rate Limiter
+const limiter = rateLimit({
+	max: 100,
+	windowMs: 60 * 60 * 1000,
+	message: "Too many requests from this IP, try again in an hour!",
+});
+
+app.use("/api", limiter);
+
+// Others
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
