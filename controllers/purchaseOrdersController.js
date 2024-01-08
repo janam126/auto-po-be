2093b8d955b83b4652b5b2f7c6dff297d9c53f13
casualme7 <miljan.pantovic@gmail.com>
2024-01-08 10:24:23 +0100

    +

--- controllers/purchaseOrdersController.js
+++ controllers/purchaseOrdersController.js
@@ -53,13 +53,14 @@ exports.createPurchaseOrder = async (req, res, next) => {
 exports.editPurchaseOrder = async (req, res, next) => {
 	try {
 		const OrderID = req.params.id;
+		
+		// Note:
+		// If you make a request with History key value pair it will only Re-Sync the event,
+		// that means it will only be updated, refreshed with all the existing properties
+		// But if you make a request without the History key value pari it will eidt the event and it will
+		// count as Updated, so new History object with info will be added to the History array
 
 		const { History, ...updatedBody } = req.body;
-
-		if (History)
-			return next(
-				new AppError("Remove History property, it will be added automatically", 400)
-			);
 
 		const updatedHistory = {
 			MissingInformation: "none",
@@ -67,16 +68,19 @@ exports.editPurchaseOrder = async (req, res, next) => {
 			UpdatedBy: req.user._id,
 		};
 
-		const order = await PurchaseOrders.findOneAndUpdate(
-			{ OrderID },
-			{
-				...updatedBody,
-				$push: {
-					History: updatedHistory,
-				},
-			},
-			{ new: true, runValidators: true }
-		);
+		const updateObject = History
+			? { ...updatedBody }
+			: {
+					...updatedBody,
+					$push: {
+						History: updatedHistory,
+					},
+			  };
+
+		const order = await PurchaseOrders.findOneAndUpdate({ OrderID }, updateObject, {
+			new: true,
+			runValidators: true,
+		});
 
 		if (!order) return next(new AppError("Purchase order doesn't exist", 400));
 
