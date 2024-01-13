2aa409eaaeab7a292079f63ac6c26a60fb40eed1
Miljan Pantovic <93261270+casualme7@users.noreply.github.com>
2024-01-13 16:10:56 +0100

    Update jwtVerification.js

--- utils/jwtVerification.js
+++ utils/jwtVerification.js
@@ -4,9 +4,9 @@ const jwtVerification = (token, settings) => {
 	return jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
 		if (err) {
 			if (err.name === "TokenExpiredError")
-				throw new Error(settings.error ?? "Your token has expired");
+				throw new Error(settings?.error ?? "Your token has expired");
 			if (err.name === "JsonWebTokenError")
-				throw new Error(settings.invalid ?? "You provided an INVALID token");
+				throw new Error(settings?.invalid ?? "You provided an INVALID token");
 			throw new Error(err.message);
 		}
 		if (decoded) return decoded;
